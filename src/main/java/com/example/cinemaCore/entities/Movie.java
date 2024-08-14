d31d18899d82cc2f258329497fcd5477588e13e8
phdangg <dangmanhphuc2003@gmal.com>
2024-08-14 13:12:42 +0700

    movie entity

---
+++ src/main/java/com/example/cinemaCore/entities/Movie.java
@@ -0,0 +1,43 @@
+package com.example.cinemaCore.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer movieId;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Movie's title should not be empty")
+    private String title;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Movie's director should not be empty")
+    private String director;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Movie's studio should not be empty")
+    private String studio;
+
+    @ElementCollection
+    @CollectionTable(name = "movie_cast")
+    private Set<String> moiveCast;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Movie's release year should not be empty")
+    private Integer releaseYear;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Movie's poster should not be empty")
+    private String poster;
+}
