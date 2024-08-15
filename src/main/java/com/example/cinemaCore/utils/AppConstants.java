5931333d08e0a103b60cf4af737191649a7568b3
phdangg <dangmanhphuc2003@gmal.com>
2024-08-15 20:14:24 +0700

    AppConstants

---
+++ src/main/java/com/example/cinemaCore/utils/AppConstants.java
@@ -0,0 +1,8 @@
+package com.example.cinemaCore.utils;
+
+public class AppConstants {
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "3";
+    public static final String SORT_DIR = "acs";
+    public static final String SORT_BY = "movieId";
+}
